a23973b6a82c8cdb01605e5742bdb19b50d785aa
Atharva Tagalpallewar <tagalpallewaratharva@gmail.com>
2020-12-08 15:09:54 +0530

    Updated cryptoScraper to add data to a csv file
    I have updated cryptoScraper so that it adds time and OHLCV data to a sample csv file in Data subdirectory which is updated every minute for five minutes. I have also changed the API to cryptoCompare as the previous one didn;t provide OHLCV data.

--- CryptoPy/cryptoScraper.py
+++ CryptoPy/cryptoScraper.py
@@ -1,34 +1,54 @@
-# API used = Nomics
+# API used = CryptoCompare
 
 import requests
 from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
+import csv
+import asyncio
 
 
-url = 'https://api.nomics.com/v1/currencies/ticker'
+url_for_minute_data = 'https://min-api.cryptocompare.com/data/histominute'
+url_for_hour_data = 'https://min-api.cryptocompare.com/data/histohour'
 
-key = 'ENTER YOUR API KEY'
+# curreny symbols: 'BTC' for bitcoin, 'ETH' for ethereum, 'LTC' for litecoin and 'XRP' for ripple
 Currency = 'BTC'
 
 
-# enter your api key from nomics in "key"
-# curreny symbols: 'BTC' for bitcoin, 'ETH' for ethereum, 'LTC' for litecoin and 'XRP' for ripple
-def getPrice(key, currencySymbol, interval='1hr'):
-    parameters = {
-        'key': key,
-        'ids': currencySymbol,
-        'interval': interval,
-        'convert': 'USDT'
-    }
-
-    try:
-        response = requests.get(url, params=parameters)
-        data = response.json()
-        price = [dp["price"] for dp in data]
-        return(', '.join(price))
-
-    except (ConnectionError, Timeout, TooManyRedirects) as e:
-        print(e)
+def write_to_file(symbol, data):
+    with open('Data/BTCUSDT_sample_data.csv', mode='a', newline='') as data_file:
+        data_writer = csv.writer(data_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        data_writer.writerow([data["time"], data["open"], data["high"], data["low"], data["close"], data["volumefrom"], data["volumeto"]])
 
 
-newPrice = getPrice(key, Currency)
-print(newPrice)
+async def get_minute_data_current(symbol):
+
+    while True:
+        parameters = {
+            'fsym': symbol,
+            'tsym': 'USDT',
+            'limit': '1',
+            'aggregate': '1'
+        }
+
+        try:
+            response = requests.get(url_for_minute_data, params=parameters)
+            data = response.json()["Data"][0]
+            write_to_file(symbol, data)
+            print("Executed")
+
+        except (ConnectionError, Timeout, TooManyRedirects) as e:
+            print(e)
+
+        await asyncio.sleep(60)
+
+
+def stop():
+    task.cancel()
+
+
+loop = asyncio.get_event_loop()
+loop.call_later(300, stop)
+task = loop.create_task(get_minute_data_current(Currency))
+try:
+    loop.run_until_complete(task)
+except asyncio.CancelledError:
+    pass
